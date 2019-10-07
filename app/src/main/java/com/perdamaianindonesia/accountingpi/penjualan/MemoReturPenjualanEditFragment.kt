c7b52211e56752ea6c1ce56b9a71245feb3d7ca3
hudamcr <achmad.huda02@gmail.com>
2019-10-07 17:54:11 +0700

    revisi retur penjualan

--- app/src/main/java/com/perdamaianindonesia/accountingpi/penjualan/MemoReturPenjualanEditFragment.kt
+++ app/src/main/java/com/perdamaianindonesia/accountingpi/penjualan/MemoReturPenjualanEditFragment.kt
@@ -3,21 +3,30 @@ package com.perdamaianindonesia.accountingpi.penjualan
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 
 import com.perdamaianindonesia.accountingpi.R
+import com.github.chrisbanes.photoview.PhotoView
+import com.perdamaianindonesia.accountingpi.MainActivity
+
+
 
 
 
 class MemoReturPenjualanEditFragment : Fragment() {
     private var batal: TextView? = null
     private var simpan: TextView? = null
-    private var ttd: TextView? = null
-    private var ttd2: TextView? = null
+    private var ttd: ImageView? = null
+    private var ttd2: ImageView? = null
+    private var retur:ImageView? =null
+    private var vertifikasi:ImageView? =null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -29,6 +38,15 @@ class MemoReturPenjualanEditFragment : Fragment() {
         (activity as PenjualanActivity).setActionBarTitle("EDIT MEMO RETUR")
         batal=view.findViewById(R.id.batal)
         simpan=view.findViewById(R.id.simpan)
+        retur=view.findViewById(R.id.img_retur)
+        vertifikasi=view.findViewById(R.id.img_vertikasi)
+        retur!!.setOnClickListener(View.OnClickListener {
+           dialog_retur()
+        })
+        vertifikasi=view.findViewById(R.id.img_vertikasi)
+        vertifikasi!!.setOnClickListener(View.OnClickListener {
+            dialog_vertifikasi()
+        })
         batal!!.setOnClickListener(View.OnClickListener {
             val newFragment = MemoReturPenjualanFragment()
             val transaction = activity!!.supportFragmentManager.beginTransaction()
@@ -56,5 +74,41 @@ class MemoReturPenjualanEditFragment : Fragment() {
 
 
         return view
+    }
+
+    private fun dialog_vertifikasi() {
+        val mBuilder = AlertDialog.Builder(context!!)
+        val mView = layoutInflater.inflate(R.layout.dialog_foto_retur, null)
+        val photoView =mView.findViewById<ImageView>(R.id.imageView)
+        photoView.setImageResource(R.drawable.foto_vertifikasi)
+        mBuilder.setView(mView)
+        val mDialog = mBuilder.create()
+
+        val image = mView.findViewById<ImageView>(R.id.imageXX)
+        image.setOnClickListener(View.OnClickListener { mDialog.cancel() })
+        mDialog.show()
+        mDialog.getWindow()!!.setBackgroundDrawableResource(android.R.color.transparent)
+        mDialog.getWindow()!!.setLayout(1600, LinearLayout.LayoutParams.WRAP_CONTENT)
+        val window = mDialog.getWindow()
+        window!!.setLayout(1600, LinearLayout.LayoutParams.WRAP_CONTENT)
+        mDialog.show()
+    }
+
+    private fun dialog_retur() {
+        val mBuilder = AlertDialog.Builder(context!!)
+        val mView = layoutInflater.inflate(R.layout.dialog_foto_retur, null)
+        val photoView =mView.findViewById<ImageView>(R.id.imageView)
+        photoView.setImageResource(R.drawable.foto_retur)
+        mBuilder.setView(mView)
+        val mDialog = mBuilder.create()
+
+        val image = mView.findViewById<ImageView>(R.id.imageXX)
+        image.setOnClickListener(View.OnClickListener { mDialog.cancel() })
+        mDialog.show()
+        mDialog.getWindow()!!.setBackgroundDrawableResource(android.R.color.transparent)
+        mDialog.getWindow()!!.setLayout(1600, LinearLayout.LayoutParams.WRAP_CONTENT)
+        val window = mDialog.getWindow()
+        window!!.setLayout(1800, LinearLayout.LayoutParams.WRAP_CONTENT)
+        mDialog.show()
     }
 }
--- app/src/main/java/com/perdamaianindonesia/accountingpi/penjualan/MemoReturPenjualanLihatFragment.kt
+++ app/src/main/java/com/perdamaianindonesia/accountingpi/penjualan/MemoReturPenjualanLihatFragment.kt
@@ -3,9 +3,12 @@ package com.perdamaianindonesia.accountingpi.penjualan
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 
@@ -16,8 +19,10 @@ import com.perdamaianindonesia.accountingpi.R
 class MemoReturPenjualanLihatFragment : Fragment() {
     private var print: TextView? = null
     private var hapus: TextView? = null
-    private var ttd: TextView? = null
-    private var ttd2: TextView? = null
+    private var ttd: ImageView? = null
+    private var ttd2: ImageView? = null
+    private var retur:ImageView? =null
+    private var vertifikasi:ImageView? =null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -29,6 +34,15 @@ class MemoReturPenjualanLihatFragment : Fragment() {
         (activity as PenjualanActivity).setActionBarTitle("LIHAT MEMO RETUR")
         print=view.findViewById(R.id.print)
         hapus=view.findViewById(R.id.hapus)
+        retur=view.findViewById(R.id.img_retur)
+        vertifikasi=view.findViewById(R.id.img_vertikasi)
+        retur!!.setOnClickListener(View.OnClickListener {
+            dialog_retur()
+        })
+        vertifikasi=view.findViewById(R.id.img_vertikasi)
+        vertifikasi!!.setOnClickListener(View.OnClickListener {
+            dialog_vertifikasi()
+        })
         print!!.setOnClickListener(View.OnClickListener {
             Toast.makeText(context,"Print  ", Toast.LENGTH_SHORT).show()
         })
@@ -49,5 +63,40 @@ class MemoReturPenjualanLihatFragment : Fragment() {
 
 
         return view
+    }
+    private fun dialog_vertifikasi() {
+        val mBuilder = AlertDialog.Builder(context!!)
+        val mView = layoutInflater.inflate(R.layout.dialog_foto_retur, null)
+        val photoView =mView.findViewById<ImageView>(R.id.imageView)
+        photoView.setImageResource(R.drawable.foto_vertifikasi)
+        mBuilder.setView(mView)
+        val mDialog = mBuilder.create()
+
+        val image = mView.findViewById<ImageView>(R.id.imageXX)
+        image.setOnClickListener(View.OnClickListener { mDialog.cancel() })
+        mDialog.show()
+        mDialog.getWindow()!!.setBackgroundDrawableResource(android.R.color.transparent)
+        mDialog.getWindow()!!.setLayout(1600, LinearLayout.LayoutParams.WRAP_CONTENT)
+        val window = mDialog.getWindow()
+        window!!.setLayout(1600, LinearLayout.LayoutParams.WRAP_CONTENT)
+        mDialog.show()
+    }
+
+    private fun dialog_retur() {
+        val mBuilder = AlertDialog.Builder(context!!)
+        val mView = layoutInflater.inflate(R.layout.dialog_foto_retur, null)
+        val photoView =mView.findViewById<ImageView>(R.id.imageView)
+        photoView.setImageResource(R.drawable.foto_retur)
+        mBuilder.setView(mView)
+        val mDialog = mBuilder.create()
+
+        val image = mView.findViewById<ImageView>(R.id.imageXX)
+        image.setOnClickListener(View.OnClickListener { mDialog.cancel() })
+        mDialog.show()
+        mDialog.getWindow()!!.setBackgroundDrawableResource(android.R.color.transparent)
+        mDialog.getWindow()!!.setLayout(1600, LinearLayout.LayoutParams.WRAP_CONTENT)
+        val window = mDialog.getWindow()
+        window!!.setLayout(1800, LinearLayout.LayoutParams.WRAP_CONTENT)
+        mDialog.show()
     }
 }
